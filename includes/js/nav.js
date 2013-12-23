cfc04c2a664865af88e89c9612d5c86aa3d0f302
Hua Yang <huacyang@gmail.com>
2013-12-23 13:08:54 -0500

    Updated templates, added new template, added navigation creation
    New Template: for subpages with no right sidebar
    Navigation: Will be generated via text file

---
+++ includes/js/nav.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+
+function buildNav(id, name, href) {
+	$("<li>").attr("id", id).attr("class", "hov").append(
+        $("<a>").attr("href", href).text(name)
+    ).appendTo($("#navigation"));
+}
+
+$(window).ready(function(e) {
+	// pathway to the text file
+	var filePath = "navigation.txt"
+	// XML request to the above pathway
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", filePath, false);
+	xmlhttp.send(null);
+	// parse the context of the text into an array
+	var fileContent = xmlhttp.responseText;
+	var fileArray = fileContent.split('\n')
+	var id, name, href, url;
+	// parse the current URL
+	var urlContent = document.URL
+	var urlArray = urlContent.split("/");
+	var url = (urlArray[urlArray.length-1]);
+	
+	for (var i = 0; i < fileArray.length; i++) {
+		var file = fileArray[i].split(':');
+		name = file[0].trim();
+		href = file[1].trim();
+		id = name.toLowerCase();
+		buildNav(id, name, href);
+		// set the respective nav to active
+		if (href == url)
+			document.getElementById(id).className = "active";
+	}
+
+});
--- includes/js/rbhs.js
+++ includes/js/rbhs.js
@@ -25,62 +25,15 @@ function mobile() {
     }
 }
 
-function activate(id, classes) {
-    document.getElementById(id).className = classes;
-}
-
-/*
- * Function for setting the corresponding tab on the navigation bar active
- */
-function active() {
-    var url = document.URL,
-        i = 0,
-        found = false;
-    var token = url.replace(/\./g, "/");
-    token = token.split("/");
-    for (; i < token.length; i++) {
-        var found = true;
-        switch (token[i]) {
-        case "about":
-            activate("about", "active");
-            break;
-        case "members":
-            activate("members", "active");
-            break;
-        case "committees":
-            activate("committees", "active");
-            break;
-        case "meetings":
-            activate("meetings", "active");
-            break;
-        case "minutes":
-            activate("minutes", "active");
-            break;
-        case "join":
-            activate("join", "active");
-            break;
-        case "reports":
-            activate("reports", "active");
-            break;
-        default:
-            found = false;
-        }
-        if (found) break;
-	}
-	if (!found) {
-		activate("home", "active");
-	}
-}
-
 // During load
 $(window).load(function() {
     $(window).resize(function() {
         mobile();
     });
 });
+
 // On first load
 $(window).ready(function(e) {
     mobile();
-    active();
 });
 
